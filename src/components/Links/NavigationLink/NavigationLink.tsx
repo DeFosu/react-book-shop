084daabfe9ae0c06cd85344680168d98bff17941
Yarik <57310769+DeFosu@users.noreply.github.com>
2025-04-04 13:58:56 +0300

    Enhance NavigationLink styles

--- src/components/Links/NavigationLink/NavigationLink.tsx
+++ src/components/Links/NavigationLink/NavigationLink.tsx
@@ -5,7 +5,7 @@ const NavigationLink: React.FC<LinkProps> = ({ href, children }) => {
   return (
     <a
       href={href}
-      className="text-septenary hover:text-accent transition-colors duration-150"
+      className="text-septenary hover:text-accent transition-colors duration-150 font-inter text-lg leading-6 tracking-wide"
     >
       {children}
     </a>
