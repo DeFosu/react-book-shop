f53b4c4e25e913bf4695f88ec5a9dfb38da32156
Yarik <57310769+DeFosu@users.noreply.github.com>
2025-04-04 13:31:12 +0300

    Refactor App component styles: update button sizes, enhance typography, and adjust layout spacing

--- src/App.tsx
+++ src/App.tsx
@@ -30,31 +30,32 @@ const App: React.FC = () => {
               <NavigationLink href="/">Pages</NavigationLink>
               <NavigationLink href="/">Contact Us</NavigationLink>
             </nav>
-            <Button variant="filled" size="lg">
+            <Button variant="filled" size="md">
               Order Today
             </Button>
           </div>
         </header>
-        <main className="max-w-[1300px] mx-auto px-5 py-24 flex items-center gap-16">
+        <main className="max-w-[1300px] mx-auto px-5 py-20 flex items-center gap-16">
           <div className="w-1/2">
             <div className="flex items-center gap-3.5">
               <div className="w-9 h-0.5 bg-accent inline-block" />
-              <h1 className="text-white text-xl">Welcome to Pages!!!</h1>
+              <h1 className="text-white text-2xl font-cardo tracking-wider leading-9">
+                Welcome to Pages!!!
+              </h1>
             </div>
-
-            <h2 className="text-white text-6xl">
+            <h2 className="mt-3.5 text-white text-6xl font-cardo tracking-wider leading-20">
               Your Books From <br /> The Best Writer.
             </h2>
-            <p className="text-lg text-secondary">
+            <p className="mt-6 text-lg text-secondary font-inter leading-7">
               We believe that reading books are essential to a healthy culture.
               <br />
               They’re where authors can connect with readers.
             </p>
-            <div>
-              <Button variant="filled" size="lg">
+            <div className="mt-10 flex">
+              <Button variant="filled" size="md">
                 Order Today
               </Button>
-              <Button variant="text" size="lg">
+              <Button variant="text" size="md">
                 Read Free Demo
               </Button>
             </div>
