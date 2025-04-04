7e65fbf9804d2631f5d5af9c4e60d4eaf1b4e9b3
Yarik <57310769+DeFosu@users.noreply.github.com>
2025-04-04 12:49:19 +0300

    Add Icon components for Logo, Facebook, LinkedIn, and Twitter

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,10 @@
+export type IconProps = {
+  className?: string;
+  width?: number;
+  height?: number;
+};
+
+export { default as Logo } from "./Logo/Logo";
+export { default as Facebook } from "./Facebook/Facebook";
+export { default as LinkedIn } from "./LinkedIn/LinkedIn";
+export { default as Twitter } from "./Twitter/Twitter";
