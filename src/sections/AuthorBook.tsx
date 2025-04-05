ab32b99cbd45b19806709328bbcc6a5c350a5c20
Yarik <57310769+DeFosu@users.noreply.github.com>
2025-04-05 10:36:12 +0300

    Refactor AuthorBook component: update to use SectionHeader

--- src/sections/AuthorBook.tsx
+++ src/sections/AuthorBook.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { SectionHeader } from "../components/SectionHeader";
 
-const AuthorBook = () => {
-  return <div>AuthorBook</div>;
+const AuthorBook: React.FC = () => {
+  return (
+    <section className="bg-white">
+      <div className="max-w-[1300px] mx-auto px-5 py-20">
+        <SectionHeader title="The Author’s Book" />
+        <div className="flex items-center gap-14"></div>
+      </div>
+    </section>
+  );
 };
 
 export default AuthorBook;
