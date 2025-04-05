2ba59930a22a195f3e27153cdb2947eb2b6966f2
Yarik <57310769+DeFosu@users.noreply.github.com>
2025-04-05 10:20:44 +0300

    Add AuthorBook section to App component: create new AuthorBook component and include it in the main application layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./sections/Home";
+import AuthorBook from "./sections/AuthorBook";
 
 const App: React.FC = () => {
   return (
     <div>
       <Home />
+      <AuthorBook />
     </div>
   );
 };
---
+++ src/sections/AuthorBook.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthorBook = () => {
+  return <div>AuthorBook</div>;
+};
+
+export default AuthorBook;
