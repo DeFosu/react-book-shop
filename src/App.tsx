02e315e0f2a54425b32e704193f4a830a3c67e75
Yarik <57310769+DeFosu@users.noreply.github.com>
2025-04-05 09:41:23 +0300

    Refactor App component to replace header and main content with Home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,10 @@
 import React from "react";
-import { Logo, Facebook, Twitter, LinkedIn } from "./components/Icons";
-import { IconLink, NavigationLink } from "./components/Links";
-import { Button } from "./components/Button";
-import { Typography } from "./components/Typography";
+import Home from "./sections/Home";
 
 const App: React.FC = () => {
   return (
     <div>
-      <section className="bg-primary">
-        <header className="max-w-[1300px] mx-auto h-28 flex items-center justify-between">
-          <div className="flex items-center gap-9">
-            <a href="/" className="block w-fit">
-              <Logo />
-            </a>
-            <ul className="flex gap-2.5 items-center">
-              <IconLink href="/">
-                <Facebook />
-              </IconLink>
-              <IconLink href="/">
-                <Twitter />
-              </IconLink>
-              <IconLink href="/">
-                <LinkedIn />
-              </IconLink>
-            </ul>
-          </div>
-          <div className="flex gap-20 items-center">
-            <nav className="flex gap-9.5 items-center">
-              <NavigationLink href="/">Home</NavigationLink>
-              <NavigationLink href="/">About</NavigationLink>
-              <NavigationLink href="/">Pages</NavigationLink>
-              <NavigationLink href="/">Contact Us</NavigationLink>
-            </nav>
-            <Button variant="filled" size="md">
-              Order Today
-            </Button>
-          </div>
-        </header>
-        <main className="max-w-[1300px] mx-auto px-5 py-20 flex items-center gap-16">
-          <div className="w-1/2">
-            <div className="flex items-center gap-3.5">
-              <div className="w-9 h-0.5 bg-accent inline-block" />
-              <Typography variant="h5" color="white">
-                Welcome to Pages!!!
-              </Typography>
-            </div>
-            <Typography variant="h1" color="white" className="mt-3.5">
-              Your Books From <br /> The Best Writer.
-            </Typography>
-            <Typography variant="body1" color="secondary" className="mt-6">
-              We believe that reading books are essential to a healthy culture.
-              <br />
-              They're where authors can connect with readers.
-            </Typography>
-            <div className="mt-10 flex">
-              <Button variant="filled" size="md">
-                Order Today
-              </Button>
-              <Button variant="text" size="md">
-                Read Free Demo
-              </Button>
-            </div>
-          </div>
-          <div className="w-1/2">
-            <img src="/images/home.jpg" alt="The dark light" />
-          </div>
-        </main>
-      </section>
+      <Home />
     </div>
   );
 };
